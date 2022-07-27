d894ac5aa81e9405982274456fc1389061520146
shayne.dcs <shayne.dcs@gmail.com>
2022-07-26 18:35:03 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-
+import Navbar from './components/Nav Bar/navbar'
 
 function App() {
   return (
-    <header>
-      <h1>Derek Shayne</h1>
-      <p>About Me</p>
-      <p>Portfolio</p>
-      <p>Contact</p>
-      <p>Resume</p>
-    </header>
-
+    <>
+      <Navbar />
+    </>
   );
 }
 
